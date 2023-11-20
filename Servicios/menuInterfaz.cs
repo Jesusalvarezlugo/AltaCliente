38a87fb9800200c9a9e0f7a79ec5010a89b79816
Jesús <csi22-jalvugo@colegioaltair.net>
2023-11-20 13:15:48 +0100

    no message

--- Servicios/menuInterfaz.cs
+++ Servicios/menuInterfaz.cs
@@ -13,5 +13,12 @@ namespace AltaCliente.Servicios
         public int MostrarMenuYSeleccion();
 
         public string NuevoAltaCliente();
+
+        //una funcionalidad a partir de esta arquitectura.
+        //crear funcionalidad de dar de alta una nueva cuenta bancaria
+        //particularidad: dentro de cuenta bancaria tiene que haber un campo que identifique a que cliente 
+        //pertenece esa cuenta bancaria(PK=FK)
+        //campos: id, isban(identificador cuenta bancaria),codigo switch(constante), fch alta,fch baja,
+        //campo indique a cliente
     }
 }
